61b79ad18502ce985fea53b4fc74e8d9fb71b845
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-04-28 21:13:42 +0530

    Create Best Time to Buy and Sell Stock with Cooldown.cpp

---
+++ Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+
+int getAns(vector<int> Arr, int ind, int buy, int n, vector<vector<int>> &dp ){
+
+    if(ind>=n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind]+getAns(Arr,ind+1,1,n,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind]+getAns(Arr,ind+2,0,n,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
+
+
+int stockProfit(vector<int> &Arr)
+{
+    int n = Arr.size();
+    vector<vector<int>> dp(n,vector<int>(2,-1));
+    
+    int ans = getAns(Arr,0,0,n,dp);
+    return ans;
+}
