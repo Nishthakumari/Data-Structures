b319695b96604832ea11d4f21021a50e5b3908ba
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-04-28 20:47:15 +0530

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/longest-common-subsequence/description/
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int m= text1.size(), n=text2.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0 || j==0) continue;
+                else if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j] =1+dp[i-1][j-1];
+                }else
+                {
+                    dp[i][j] =max( dp[i-1][j],dp[i][j-1] );
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
