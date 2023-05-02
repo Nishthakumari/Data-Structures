89f977fee556ead328b9d0b2444d8e56165f5064
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-05-02 15:12:31 +0530

    Create  Coin Change II.cpp

---
+++ Coin Change II.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/coin-change-ii/solutions/501113/coin-change-2-c-recursive-memoization-and-tabulation-method/?orderBy=most_votes
+//https://leetcode.com/problems/coin-change-ii/
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+
+        if(amount==0) return 1;
+        if(n==0) return 0;
+
+        vector<vector<int>> dp(n+1, vector<int>(amount+1,-1));
+
+        for(int i=0;i<n+1;i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for(int i=0;i<amount+1;i++)
+        {
+            dp[0][i]= 0;
+        }
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]>j)
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+            }
+        }
+
+        return dp[n][amount];
+
+
+    }
+};
