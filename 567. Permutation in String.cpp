d6b96e1d47c660a0046d89a7a828d9e700398b04
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-05-06 13:52:20 +0530

    Update 567. Permutation in String.cpp

--- 567. Permutation in String.cpp
+++ 567. Permutation in String.cpp
@@ -9,46 +9,44 @@
 
 class Solution {
 public:
-    bool checkInclusion(string p, string s)
-    {
-        unordered_map<char,int>m;
-        int n=s.length();
-        int k=p.length();
-        for(auto i:p)
-            m[i]++;
-        int c=m.size();
-        int i=0,j=0;
-        while(j<n)
-        {
-            if(m.count(s[j]))
-            {
-                m[s[j]]--;
-               if(m[s[j]]==0)
-                   c--;
-            }
-            
-            if((j-i+1) <k)
-                j++;
-            
-            else if((j-i+1) ==k)
-            {
-                if(c==0)
-                    return true;
-                
-                if(m.count(s[i]))
-                {
-                  m[s[i]]++;
-                    
-                  if (m[s[i]]==1)
-                  c++;
-                        
-                }
-                          i++;
-                          j++;
-            }
-           
-        }
-           return false;
-        
+    bool checkInclusion(string pat, string txt) {
+
+  unordered_map <char, int> m;
+  for(auto i: pat)
+  m[i]++;
+
+  int k = pat.size();
+  int count = m.size();
+
+  int i = 0, j = 0;
+
+  while(j < txt.size()) {
+
+  if(m.find(txt[j]) != m.end()) {
+  m[txt[j]]--;
+
+  if(m[txt[j]] == 0)
+  count--;
+  }
+
+  if(j - i + 1 < k) j++;
+
+  else if(j - i + 1 == k) {
+  if(count == 0)
+ return true;
+
+  if(m.find(txt[i]) != m.end()) {
+  m[txt[i]]++;
+
+  if(m[txt[i]] == 1)
+  count++;
+  }
+
+  i++; j++;
+  }
+  }
+
+ return false;
+
     }
 };
