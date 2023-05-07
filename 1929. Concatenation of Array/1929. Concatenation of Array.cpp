b0ff050ce56c42d8e9aa9fd932936e21772205ff
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-05-07 16:51:03 +0530

    Create 1929. Concatenation of Array.cpp

---
+++ 1929. Concatenation of Array/1929. Concatenation of Array.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/concatenation-of-array/
+
+class Solution{    
+    public:    
+        vector<int> getConcatenation(vector<int>& nums){            
+            vector<int> ans;            
+            int len;            
+            len = nums.size();            
+            for(int i = 0; i < 2 * len; i++){                
+                ans.push_back(nums[i % len]);                
+            }
+            return ans;            
+        }
+};
