ccc0a61b87883fd577400592f0db1905b5e150ab
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-05-08 17:43:33 +0530

    Update 377. Combination Sum IV.cpp

--- 377. Combination Sum IV.cpp
+++ 377. Combination Sum IV.cpp
@@ -24,3 +24,34 @@ public:
         return dp[target];
     }
 };
+
+
+// With Recusion
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+       vector<int> dp(target+1, -1);
+       dp[0]=1;
+       cs(nums, target, dp);
+       return dp[target];
+
+    }
+
+    int cs(vector<int> &nums, int target, vector<int> &dp)
+    {
+       
+        if(dp[target]>-1)  return dp[target];
+
+        int count = 0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(target-nums[i]>=0)
+            count += cs(nums, target-nums[i], dp);
+        }
+
+        return dp[target]=count;
+    }
+};
+
