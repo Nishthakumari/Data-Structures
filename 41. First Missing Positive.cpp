5835ae41db48db8e0570211bccd173f6e641f467
Nishtha kumari <51745210+Nishthakumari@users.noreply.github.com>
2023-05-18 08:53:56 +0530

    Create 41. First Missing Positive.cpp

---
+++ 41. First Missing Positive.cpp
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/first-missing-positive/description/
+
+
+Put each number in its right place.
+
+For example:
+
+When we find 5, then swap it with A[4].
+
+At last, the first place where its number is not right, return the place + 1. */
+
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& A) {
+        int n=A.size();
+        for(int i = 0; i < n; ++ i)
+            while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
+                swap(A[i], A[A[i] - 1]);
+        
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
+    }
+    
+    
+};
